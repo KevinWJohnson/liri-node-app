e4b9c5b210fe3b2b05e98e0d3e277cb76efe1562
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-09 08:56:33 -0700

    Added Else structure to liri.js

--- liri.js
+++ liri.js
@@ -11,3 +11,44 @@ var client = new Twitter(keys.twitter);
 // `spotify-this-song`
 // `movie-this`
 // `do-what-it-says`
+
+
+// Takes in all of the command line arguments
+var inputString = process.argv;
+
+// Caputures the command line argument
+var liriCommand = inputString[2];
+
+// Caputures the command line parameter
+var liriParameter = inputString[3];
+
+
+// Determines the command selected...
+// Based on the command we run the appropriate set of commands
+if (operand === "my-tweets") {
+  // Commands for Twitter
+}
+
+else if (operand === "spotify-this-song") {
+  // Commands for Spotify
+}
+
+else if (operand === "movie-this") {
+  // Commands for OMDB
+}
+
+else if (operand === "do-what-it-says") {
+  // fs Node package uses random.txt to
+  // call one of LIRI's commands
+}
+
+
+else {
+  notRecognized = "Not a recognized command";
+}
+
+
+// Prints the outputNumber
+console.log(notRecognized);
+
+// =======================================================

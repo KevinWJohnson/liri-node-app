97ab0a43fcb524715ead1ebce8b0165194125c29
KevinWJohnson <kevin.johnson3@cox.net>
2018-07-10 21:07:52 -0700

    Fixed README file

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 
    * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-   3. `node liri.js movie-this '<movie name here>'`
+3. `node liri.js movie-this '<movie name here>'`
 
    * This will output the following information to your terminal/bash window:
 
@@ -34,7 +34,7 @@
 
     * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
      
-     4. `node liri.js do-what-it-says`
+4. `node liri.js do-what-it-says`
    
      * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
      
---
+++ liri_old2.js
@@ -0,0 +1,284 @@
+
+// Code to read and set any environment variables with the dotenv package
+require("dotenv").config();
+
+// Include the twitter npm package
+var Twitter = require("twitter");
+
+// Include the twitter npm package
+var Spotify = require("node-spotify-api");
+
+// Include the request npm package
+var request = require("request");
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// Make liri.js take in one of the following commands
+//
+// `my-tweets`
+// `spotify-this-song`
+// `movie-this`
+// `do-what-it-says`
+
+
+// Takes in all of the command line arguments
+var nodeArgs = process.argv;
+
+// Caputures the command line argument
+var liriCommand = nodeArgs[2];
+
+// Caputures the Twitter User
+var userTwitter = process.argv[3];
+
+
+// Determines the command selected...
+// Based on the command we run the appropriate set of commands
+if (liriCommand === "my-tweets") {
+  myTweetsListed();
+}
+
+else if (liriCommand === "spotify-this-song") {
+  songInfo();
+
+}
+
+else if (liriCommand === "movie-this") {
+  movieInfo();
+}
+
+else if (liriCommand === "do-what-it-says") {
+  fileReadCommand();
+}
+
+else {
+  console.log("Not a recognized command");
+}
+
+
+// ----------- Functions -----------
+
+function myTweetsListed() {
+
+  // Commands for Twitter
+
+  // Twitter User Name: CBCHWLIRI
+
+  // Twitter get command
+  var params = { screen_name: userTwitter };
+
+  client.get('statuses/user_timeline', params, function (error, tweets, response) {
+
+    if (!error) {
+      var tweetFrom = "\nTweets from: @" + tweets[0].user.screen_name;
+      console.log(tweetFrom);
+
+      for (var i = 0; i < tweets.length; i++) {
+        var twitterResponse = "\n---------\n" + "Tweet: " + tweets[i].text + "\nDate of Tweet " + tweets[i].created_at;
+        console.log(twitterResponse);
+      }
+    }
+  });
+}
+
+
+function songInfo() {
+  var songName = "";
+
+  // Get the Song Name
+  // Loop through all the words in the node argument
+  for (var i = 3; i < nodeArgs.length; i++) {
+    songName = songName + " " + nodeArgs[i];
+  }
+
+  // Taking off the space in front of the song name
+  songName = songName.substr(1);
+  console.log("SongName: " + songName)
+
+  // Commands for Spotify
+
+  // If no song name was entered default to "The Sign" by the Ace of Base
+  if (songName === "") {
+
+    songName = "The Sign"
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      //console.log(JSON.stringify(data,null,2));
+
+      console.log("---------------- Song Data Below ----------------");
+      //console.log("data.length: "+ data.length);
+      //console.log("data: "+ data);
+      //console.log("data.tracks.items.length: "+ data.tracks.items.length);
+      //console.log("data.tracks.items.length: "+ data.tracks.items.length);
+
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var j = i + 1;
+
+
+        for (var k = 0; k < data.tracks.items[i].album.artists.length; k++) {
+          if (data.tracks.items[i].album.artists[k].name === "Ace of Base") {
+            console.log("------Default Song Data --------")
+            console.log("Song Name Searched: " + "'" + songName.toUpperCase() + "'");
+            console.log("Song Name Found: " + data.tracks.items[i].name);
+            console.log("Album Name: " + data.tracks.items[i].album.name);
+            console.log("Artist Name: " + data.tracks.items[i].album.artists[k].name);
+            console.log("Preview Link: " + data.tracks.items[i].preview_url);
+          }
+
+        }
+
+
+        // console.log("URL: " + data.tracks.items[i].album.external_urls.spotify);
+
+      }
+    });
+  }
+  else {
+
+    //console.log(nodeArgs.length);
+    //console.log(songName);
+    // spotify.search({ type: 'track', query: 'You are my sunshine', limit: 1 }, function (err, data) {
+
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      //console.log(JSON.stringify(data, null, 2));
+
+      console.log("---------------- Song Data Below ----------------");
+
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var j = i + 1;
+        console.log("-------Song Data Number " + j + "--------")
+        console.log("Song Name Searched: " + "'" + songName.toUpperCase() + "'");
+        console.log("Song Name Found: " + data.tracks.items[i].name);
+        console.log("Album Name: " + data.tracks.items[i].album.name);
+
+        for (var k = 0; k < data.tracks.items[i].album.artists.length; k++) {
+          console.log("Artist(s) Name: " + data.tracks.items[i].album.artists[k].name);
+        }
+
+        console.log("Preview Link: " + data.tracks.items[i].preview_url);
+      }
+    });
+  }
+}
+
+function movieInfo() {
+  // Commands for OMDB
+
+  // Create an empty variable for holding the movie name
+  var movieName = "";
+
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 3; i < nodeArgs.length; i++) {
+    if (i > 3 && i < nodeArgs.length) {
+      movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+      movieName += nodeArgs[i];
+    }
+  }
+
+  // Setting up the default movie - if no movie is entered
+  if (movieName === "") {
+    movieName = "Mr. Nobody"
+  }
+
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+  // This line is just to help us debug against the actual URL.
+  //console.log(queryUrl);
+
+  request(queryUrl, function (error, response, body) {
+
+    //console.log(JSON.stringify(body, null, 2));
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+      console.log("---------------- Movie Data Below ----------------");
+      console.log("Title of the movie: " + JSON.parse(body).Title);
+      console.log("Year the movie came out: " + JSON.parse(body).Year);
+      console.log("IMBD Rating of the movie: " + JSON.parse(body).imdbRating);
+
+
+      //console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings.Source[1].Value);
+      console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+      console.log("The country where the movie was produced: " + JSON.parse(body).Country);
+      console.log("Language of the movie: " + JSON.parse(body).Language);
+      console.log("Plot of the movie: " + JSON.parse(body).Plot);
+      console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    }
+  });
+}
+
+function fileReadCommand() {
+  // fs Node package uses random.txt to
+  // call one of LIRI's commands
+
+  // fs is a core Node package for reading and writing files
+  var fs = require("fs");
+
+  // This block of code will read from the "random.txt" file.
+  // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+  // The code will store the contents of the reading inside the variable "data"
+  fs.readFile("random.txt", "utf8", function (error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    // We will then print the contents of data
+    //console.log(data);
+
+    // Then split it by commas to get songName
+    var dataArr = data.split(",");
+
+    // We will then re-display the content as an array for later use.
+    //console.log(dataArr);
+
+    //console.log("dataArr[1]: " + dataArr[1]);
+
+    // setting the songName
+    songName = dataArr[1];
+
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      //console.log(JSON.stringify(data, null, 2));
+
+      console.log("---------------- Song Data Below ----------------");
+
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var j = i + 1;
+        console.log("-------Song Data Number " + j + "--------")
+        console.log("Song Name Searched: " + "'" + songName.toUpperCase() + "'");
+        console.log("Song Name Found: " + data.tracks.items[i].name);
+        console.log("Album Name: " + data.tracks.items[i].album.name);
+
+        for (var k = 0; k < data.tracks.items[i].album.artists.length; k++) {
+          console.log("Artist(s) Name: " + data.tracks.items[i].album.artists[k].name);
+        }
+
+        console.log("Preview Link: " + data.tracks.items[i].preview_url);
+      }
+    });
+  });
+}
